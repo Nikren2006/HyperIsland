898067c2a3aec8376f6cd29b1dc47c75010c658c
1812z <2023158207@qq.com>
2026-05-10 23:58:55 +0800

    feat：过滤mipush无用文本

--- android/app/src/main/kotlin/io/github/hyperisland/xposed/template/core/filters/KeywordFilter.kt
+++ android/app/src/main/kotlin/io/github/hyperisland/xposed/template/core/filters/KeywordFilter.kt
@@ -7,10 +7,18 @@ import io.github.hyperisland.xposed.template.core.models.NotifData
 object KeywordFilter {
 
     private const val TAG = "HyperIsland[KeywordFilter]"
+    private const val GROUP_SUMMARY_TITLE = "新消息 GroupSummary"
+    private const val GROUP_SUMMARY_BODY = "你有一条新消息"
 
     fun shouldBlock(data: NotifData): Boolean {
         val pkg = data.pkg
         val channelId = data.channelId
+
+        val normalizedTitle = normalizeNotificationText(data.title)
+        val normalizedSubtitle = normalizeNotificationText(data.subtitle)
+        if (normalizedTitle == GROUP_SUMMARY_TITLE && normalizedSubtitle == GROUP_SUMMARY_BODY) {
+            return true
+        }
 
         val mode = ConfigManager.getString(
             "pref_channel_filter_mode_${pkg}_$channelId",
@@ -51,4 +59,10 @@ object KeywordFilter {
 
         return result
     }
+
+    private fun normalizeNotificationText(value: String): String = value
+        .replace('\u00A0', ' ')
+        .replace("\u200B", "")
+        .replace(Regex("\\s+"), " ")
+        .trim()
 }
